f89d661f62a479393d7c103987f23ecf4a963e17
vina <alvina.angelin.s@gmail.com>
2023-09-27 00:29:19 +1300

    Added css to the submission form

--- frontend/components/ArticleSubmissionForm.tsx
+++ frontend/components/ArticleSubmissionForm.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { Article } from "../src/schema/article"
+import React, { useState } from 'react';
+import { Article } from "../src/schema/article";
+import styles from './SubmissionForm.module.css';
 
 type Props = { 
   saveArticle: (e: React.FormEvent, formData: Article | unknown) => void;
@@ -16,57 +17,62 @@ const ArticleSubmissionForm: React.FC<Props> = ({ saveArticle }) => {
   }
 
   return (
-    <form className='Form' onSubmit={(e) => saveArticle(e, formData)}>
-          <label>
-            Article Title:
-            <input onChange={handleForm} type="text" id="title" />
+    <form className={styles.Form} onSubmit={(e) => saveArticle(e, formData)}>
+      <div className={styles.FormContent}>
+        <div className={styles.LeftColumn}>
+          <label> Article Title:
+            <input className={styles.Input} onChange={handleForm} type="text" id="title" />
           </label>
           <br/>
-          <label>
-            Author:
-            <input onChange={handleForm} type="text" id="author" />
+          <label> Author:
+            <input className={styles.Input} onChange={handleForm} type="text" id="author" />
             <button type="button">+</button>
           </label>
           <br/>
-          <label>
-            Keywords:
-            <input onChange={handleForm} type="text" id="keywords" />
+          <label> Keywords:
+            <input className={styles.Input} onChange={handleForm} type="text" id="keywords" />
           </label>
           <br/>
-          <label>
-            Journal:
-            <input onChange={handleForm} type="text" id="journal" />
+          <label> Abstract:
+           <input className={styles.Input} onChange={handleForm} type="text" id="abstract" />
+          </label>
+        </div>
+        <div className={styles.RightColumn}>
+          <label> Journal:
+            <input className={styles.Input} onChange={handleForm} type="text" id="journal" />
           </label>
           <br/>
-          <label>
-            Date:
-            <input onChange={handleForm} type="date" id="date" />
+          <div className={styles.RightColumnRow}>
+          <label> Date:
+            <input className={styles.Input} onChange={handleForm} type="date" id="date" />
           </label>
           <br/>
-          <label>
-            DOI:
-            <input onChange={handleForm} type="text" id="doi" />
+          <label> DOI:
+            <input className={styles.Input} onChange={handleForm} type="text" id="doi" />
           </label>
           <br/>
-          <label>
-            Volume:
-            <input onChange={handleForm} type="number" id="volume" />
+          </div>
+          <div className={styles.RightColumnRow}>
+          <label> Volume:
+            <input className={styles.Input} onChange={handleForm} type="number" id="volume" />
           </label>
           <br/>
-          <label>
-            Issue:
-            <input onChange={handleForm} type="number" id="issue" />
+          <label> Issue:
+            <input className={styles.Input} onChange={handleForm} type="number" id="issue" />
           </label>
           <br/>
-          <label>
-            Page Range:
-            <input onChange={handleForm} type="number" id="rangeone" />
-            <input onChange={handleForm} type="number" id="rangetwo" />
+          </div>
+          <div className={styles.RightColumnRow}>
+          <label> Page Range 1:
+            <input className={styles.Input} onChange={handleForm} type="number" id="rangeone" />
           </label>
-          <br/>
-          <label>
-            <button disabled={formData === undefined ? true : false} type="submit">Submit</button>
+          <label> Page Range 2:
+            <input className={styles.Input} onChange={handleForm} type="number" id="rangetwo" />
           </label>
+          </div>
+        </div>
+      </div>
+          <button disabled={formData === undefined ? true : false} type="submit">Submit</button>
     </form>
   )
 }
