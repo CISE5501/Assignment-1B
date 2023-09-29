35b90e97919b5f2cb23cb9770fa54120ebebce74
nate <106148472+natevaut@users.noreply.github.com>
2023-09-29 14:14:07 +1300

    Configure CORS in backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(); // allow all origins
   await app.listen(3000);
 }
 bootstrap();
