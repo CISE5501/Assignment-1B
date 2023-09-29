5d4817dc287d5123ddcb2105c8c8ae3a61b25f22
nate <jmy3092@autuni.ac.nz>
2023-09-29 13:02:00 +1300

    Reformat all files with prettier

--- frontend/components/table/SortableTable.tsx
+++ frontend/components/table/SortableTable.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Table } from 'react-bootstrap';
 
 export type ComputedRow<T> = {
-  computed: true,
-  label: string,
-  content: (rowData: T) => React.JSX.Element | string,
-}
+  computed: true;
+  label: string;
+  content: (rowData: T) => React.JSX.Element | string;
+};
 
 export type DataRow<T> = {
-  label: string,
-  key: keyof T,
-  displayAs?: (data: any) => React.JSX.Element | string,
-}
+  label: string;
+  key: keyof T;
+  displayAs?: (data: any) => React.JSX.Element | string;
+};
 
 export type SortableTableHeader<T> = ComputedRow<T> | DataRow<T>;
 
@@ -22,22 +22,32 @@ interface SortableTableProps<T> {
 
 //retrieves data sorted as a table
 const SortableTable = <T,>({ headers, data }: SortableTableProps<T>) => (
-  <Table className="md-5" >
+  <Table className="md-5">
     <thead>
       <tr>
         {headers.map((header, j) => (
-          <th key={'key' in header ? header.key.toString() : 'computed' + j}>{header.label}</th>
+          <th key={'key' in header ? header.key.toString() : 'computed' + j}>
+            {header.label}
+          </th>
         ))}
       </tr>
     </thead>
     <tbody data-testid="data-table-body">
       {data.map((row, i) => (
         <tr key={i}>
-          {headers.map((header, j) => (
-            'computed' in header
-              ? <td key={'computed' + j}>{header.content(row)}</td>
-              : <td key={header.key.toString()}>{header.displayAs ? header.displayAs(row[header.key]) : <span>{row[header.key]}</span>}</td>
-          ))}
+          {headers.map((header, j) =>
+            'computed' in header ? (
+              <td key={'computed' + j}>{header.content(row)}</td>
+            ) : (
+              <td key={header.key.toString()}>
+                {header.displayAs ? (
+                  header.displayAs(row[header.key])
+                ) : (
+                  <span>{row[header.key]}</span>
+                )}
+              </td>
+            ),
+          )}
         </tr>
       ))}
     </tbody>
--- frontend/pages/analyst/index.tsx
+++ frontend/pages/analyst/index.tsx
@@ -3,7 +3,10 @@ import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import { QueuedArticle } from '@/src/schema/queuedArticle';
-import SortableTable, { ComputedRow, DataRow } from '@/components/table/SortableTable';
+import SortableTable, {
+  ComputedRow,
+  DataRow,
+} from '@/components/table/SortableTable';
 import { handleDelete, PageProps } from '@/common/queueCommon';
 import { getServerData } from '@/common/queueCommon';
 import DOMAIN from '@/DOMAIN';
@@ -12,37 +15,65 @@ export type IndexProps = PageProps;
 export const getServerSideProps = getServerData('analyst/index');
 
 const promote = async (id: string): Promise<void> => {
-  const response = await fetch(DOMAIN + 'analyst/promote/' + id, { method: 'PUT' });
+  const response = await fetch(DOMAIN + 'analyst/promote/' + id, {
+    method: 'PUT',
+  });
   if (response.ok) {
-    alert('Successfully promoted article.')
+    alert('Successfully promoted article.');
     window.location.reload();
   } else {
     alert('Failed to promote article.');
   }
-}
+};
 
 const Index = ({ queueData, duplicates }: PageProps) => {
-  const headersList: ((DataRow<QueuedArticle> & { key: keyof QueuedArticle }) | ComputedRow<QueuedArticle>)[] = [
+  const headersList: (
+    | (DataRow<QueuedArticle> & { key: keyof QueuedArticle })
+    | ComputedRow<QueuedArticle>
+  )[] = [
     { key: 'title', label: 'Title' },
-    { key: 'authors', label: 'Authors', displayAs: (authors) => authors.join('; ') },
+    {
+      key: 'authors',
+      label: 'Authors',
+      displayAs: (authors) => authors.join('; '),
+    },
     { key: 'date', label: 'Date' },
     { key: 'journal', label: 'Journal' },
     { key: 'volume', label: 'Volume' },
     { key: 'issue', label: 'Issue' },
-    { key: 'pageRange', label: 'Page Range', displayAs: ([start, end]) => (start + '-' + end) },
-    { key: 'doi', label: 'DOI' },
-    { key: 'keywords', label: 'Keywords', displayAs: (keywords) => keywords.join(', ') },
-    { key: 'abstract', label: 'Abstract' },
-    { computed: true, label: 'Warnings', content: (data) => (duplicates.includes(data.doi) ? <strong>Duplicate</strong> : '') },
     {
-      computed: true, label: 'Actions', content: (data) => (
+      key: 'pageRange',
+      label: 'Page Range',
+      displayAs: ([start, end]) => start + '-' + end,
+    },
+    { key: 'doi', label: 'DOI' },
+    {
+      key: 'keywords',
+      label: 'Keywords',
+      displayAs: (keywords) => keywords.join(', '),
+    },
+    { key: 'abstract', label: 'Abstract' },
+    {
+      computed: true,
+      label: 'Warnings',
+      content: (data) =>
+        duplicates.includes(data.doi) ? <strong>Duplicate</strong> : '',
+    },
+    {
+      computed: true,
+      label: 'Actions',
+      content: (data) => (
         <div>
-          <button type="button" onClick={() => handleDelete('queue', data)}>Delete</button>
+          <button type="button" onClick={() => handleDelete('queue', data)}>
+            Delete
+          </button>
           <br />
-          <button type="button" onClick={() => promote(data._id)}>Mark Analysed</button>
+          <button type="button" onClick={() => promote(data._id)}>
+            Mark Analysed
+          </button>
         </div>
-      )
-    }
+      ),
+    },
   ];
 
   return (
@@ -52,7 +83,11 @@ const Index = ({ queueData, duplicates }: PageProps) => {
       <h1>Analyst View</h1>
       <h2>Articles in Queue Pending Analysis</h2>
       <SortableTable headers={headersList} data={queueData.articleData} />
-      {queueData.articleData.length === 0 ? <strong>No Articles Needing Analysis</strong> : ''}
+      {queueData.articleData.length === 0 ? (
+        <strong>No Articles Needing Analysis</strong>
+      ) : (
+        ''
+      )}
     </Container>
   );
 };
