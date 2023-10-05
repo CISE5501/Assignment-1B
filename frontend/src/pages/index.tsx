bf52c65d3217211f39c505691a3ae18dc7b70ac1
vina <alvina.angelin.s@gmail.com>
2023-10-05 15:21:58 +1300

    added search bar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,13 @@ export default function Home() {
   return (
     <div className="container">
       <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+      <h2>Search for Keywords</h2>
+      <form action={"/search"}>
+        <input type="text" name="keywords" />
+        <button type="submit">
+          search
+        </button>
+      </form>
     </div>
   );
 }
