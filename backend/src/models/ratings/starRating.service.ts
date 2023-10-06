1144f80aa38c24cdac01cae724e402f9ec930013
nate <jmy3092@autuni.ac.nz>
2023-10-06 13:25:28 +1300

    Fill out star rating backend service

--- backend/src/models/ratings/starRating.service.ts
+++ backend/src/models/ratings/starRating.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IStarRating } from './starRating.interface';
 import { CreateRatingDto } from './dto/create-rating.dto';
+import { UpdateRatingDto } from './dto/update-rating.dto';
 
 @Injectable()
 export class StarRatingService {
@@ -11,5 +12,39 @@ export class StarRatingService {
   async createRating(createRatingDto: CreateRatingDto): Promise<IStarRating> {
     const newRating = await new this.articleModel(createRatingDto);
     return newRating.save();
+  }
+
+  async updateRating(updateRatingDto: UpdateRatingDto): Promise<IStarRating> {
+    const { doi, userId } = updateRatingDto;
+    const newRating = await this.articleModel.findOneAndUpdate({ doi, userId }, updateRatingDto);
+    return newRating;
+  }
+
+  async getAllRatings(): Promise<IStarRating[]> {
+    const allRatings = await this.articleModel.find();
+    return allRatings;
+  }
+
+  async getRatingsData(doi: string): Promise<IStarRating[]> {
+    const allRatings = await this.getAllRatings();
+    const filteredRatings = allRatings.filter((data) => data.doi === doi);
+    return filteredRatings;
+  }
+
+  async getIndividualRatings(doi: string): Promise<number[]> {
+    const ratingsData = await this.getRatingsData(doi);
+    const starRatings = ratingsData.map((data) => data.rating);
+    return starRatings;
+  }
+
+  async getAverageRating(doi: string): Promise<number | null> {
+    // Get all star ratings
+    const starRatings = await this.getIndividualRatings(doi);
+    // Return null for unrated
+    if (starRatings.length === 0) return null;
+    // Calculate average
+    const ratingsSum = starRatings.reduce((total, val) => total + val, 0);
+    const avgRating = ratingsSum / starRatings.length;
+    return avgRating;
   }
 }
