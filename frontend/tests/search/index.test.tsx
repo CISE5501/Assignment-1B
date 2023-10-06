c25e72b858acdba5cdce56ab439095df5d8dd076
nate <jmy3092@autuni.ac.nz>
2023-10-06 16:56:12 +1300

    Add tests for Search

---
+++ frontend/tests/search/index.test.tsx
@@ -0,0 +1,29 @@
+import { MemoryRouter, Route } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Search from '@/pages/search';
+
+function renderSearch(keyword: string) {
+  return render(
+    <MemoryRouter initialEntries={['/search?keywords=' + keyword]}>
+      <Route path="/search">
+        <Search />
+      </Route>
+    </MemoryRouter>,
+  );
+}
+
+test('should have search results', async () => {
+  const keyword = 'asd';
+  renderSearch(keyword);
+  expect(screen.getByText(`Results for '${keyword}'`)).toBeInTheDocument();
+  expect(screen.getByText('DOI')).toBeInTheDocument();
+  expect(screen.getByText('No Results')).not.toBeInTheDocument();
+});
+
+test('should have no search results', async () => {
+  const keyword = 'djugihdfgdfgd';
+  renderSearch(keyword);
+  expect(screen.getByText(`Results for '${keyword}'`)).toBeInTheDocument();
+  expect(screen.getByText('No Results')).toBeInTheDocument();
+});
