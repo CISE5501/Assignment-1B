d360a2862fe25b7fd632d904c401ce0be9d5d2ae
janenotjung <janejung1221@gmail.com>
2023-10-10 22:08:46 +1300

    bro PLEASE

---
+++ frontend/tests/index/index.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import Index, { ArticleProps } from '../../src/pages/articles';
+import '@testing-library/jest-dom';
+import { RatedArticle } from '../../src/pages/articles';
+
+const tempArray = [
+  {
+    title: 'sdf',
+    authors: ['sdasd'],
+    date: '0004-03-31',
+    journal: 'sdf',
+    volume: 2,
+    issue: 2,
+    pageRange: [3, 5],
+    doi: 'dsfsdfsdfsdf',
+    keywords: ['sad', 'asd'],
+    abstract: 'sfasd',
+    rating: 4,
+  },
+] as RatedArticle[];
+
+function renderHome(props: Partial<ArticleProps> = {}) {
+  const defaultProps: ArticleProps = {
+    articleData: [],
+  };
+  return render(<Index {...defaultProps} {...props} />);
+}
+
+function renderHomeWithArticles(props: Partial<ArticleProps> = {}) {
+  const defaultProps: ArticleProps = {
+    articleData: tempArray,
+  };
+  return render(<Index {...defaultProps} {...props} />);
+}
+
+test('should have table with an article entry', async () => {
+  renderHomeWithArticles();
+  expect(screen.getByRole('table')).toBeInTheDocument();
+});
+
+test('should have star rating', async () => {
+  renderHomeWithArticles();
+  expect(screen.getByTitle('4 Stars')).toBeInTheDocument();
+});
