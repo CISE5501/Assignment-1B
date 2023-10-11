f2975512921dadfa5536ff2c641fb2b2777eb6cc
janenotjung <janejung1221@gmail.com>
2023-10-11 14:18:40 +1300

    Update moderator.controller.ts
    - changed queue model to moderator since they're pretty much the same thing

--- backend/src/moderator/moderator.controller.ts
+++ backend/src/moderator/moderator.controller.ts
@@ -1,22 +1,28 @@
 import { Controller, Get, HttpStatus, Param, Put, Res, Delete } from '@nestjs/common';
+import { ArticleService } from 'src/models/articles/article.service';
 import { QueuedArticleService } from 'src/models/queuedArticles/queuedArticle.service';
+import { RejectedEntryService } from 'src/models/rejected/rejected.service';
 
 /*
   Routes paths with prefix '/moderator' in the URL to functions declared in service modules.
   This controller contains functions accessible by a moderator role:
     - Getting a list of queuedArticle objects
     - Getting a specified queuedArticle object
+    - Getting a list of duplicate dois
     - Updating a queuedArticle object's status (isModerated)
-    - Deleting a specified queuedArticle object
+    - Deleting a specified queuedArticle object (sending it to rejected collection)
 */
 
 @Controller('moderator')
 export class ModeratorController {
-  constructor(private readonly queuedArticleService: QueuedArticleService) {}
+  constructor(
+    private readonly articleService: ArticleService,
+    private readonly queuedArticleService: QueuedArticleService,
+    private readonly rejectedEntryService: RejectedEntryService,) {}
 
   // getArticles: returns list of queuedArticle objects which have NOT been moderated
   @Get('/index')
-  async getArticles(@Res() response) {
+  async getQueuedArticles(@Res() response) {
     try {
       const articleData = await this.queuedArticleService.getAllUnmoderatedArticles();
       return response.status(HttpStatus.OK).json({
@@ -42,9 +48,27 @@ export class ModeratorController {
     }
   }
 
+  // listDuplicateArticleDOIs: returns list of dois which exist in both the queuedArticles database and articles database
+  @Get('duplicates')
+  async listDuplicateArticleDOIs(@Res() response) {
+    try {
+      const accepted = await this.articleService.getAllArticles();
+      const acceptedDOIs = accepted.map((article) => article.doi);
+      const queued = await this.queuedArticleService.getAllQueuedArticles();
+      const duplicatesInQueue = queued.filter((queueArticle) => acceptedDOIs.includes(queueArticle.doi));
+      const duplicateDOIs = duplicatesInQueue.map((article) => article.doi);
+      return response.status(HttpStatus.OK).json({
+        message: 'All duplicate articles found successfully',
+        duplicateDOIs,
+      });
+    } catch (err) {
+      return response.status(err.status).json(err.response);
+    }
+  }
+
   //updates a queuedArticle object's isModerated status to true, marking it ready for analysis
   @Put('/promote/id/:id')
-  async promoteArticle(@Res() response, @Param('id') articleId: string) {
+  async promoteQueuedArticle(@Res() response, @Param('id') articleId: string) {
     try {
       const existingArticle = await this.queuedArticleService.updateQueuedArticle(articleId, { isModerated: true });
       return response.status(HttpStatus.OK).json({
@@ -61,6 +85,7 @@ export class ModeratorController {
   async deleteArticle(@Res() response, @Param('id') articleId: string) {
     try {
       const deletedArticle = await this.queuedArticleService.deleteQueuedArticle(articleId);
+      await this.rejectedEntryService.addEntry({ doi: deletedArticle.doi });
       return response.status(HttpStatus.OK).json({
         message: 'Article deleted successfully',
         deletedArticle,
