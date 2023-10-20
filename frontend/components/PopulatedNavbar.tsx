af49dfc107496479779865c9b5feeec49e53bc28
nate <jmy3092@autuni.ac.nz>
2023-10-20 16:25:18 +1300

    Revamp navbar
    Implement TA suggestions.

--- frontend/components/PopulatedNavbar.tsx
+++ frontend/components/PopulatedNavbar.tsx
@@ -1,7 +1,6 @@
-import { IoMdArrowDropdown } from 'react-icons/io';
 import NavBar from './nav/NavBar';
-import NavDropdown from './nav/NavDropdown';
 import NavItem from './nav/NavItem';
+import SearchDisplay from './search/SearchDisplay';
 
 const PopulatedNavBar = () => {
   return (
@@ -11,15 +10,9 @@ const PopulatedNavBar = () => {
         Home
       </NavItem>
       <NavItem route="/moderator">Moderator</NavItem>
-      <NavItem route="/analyst"> Analyst </NavItem>
-      <NavItem dropdown route="/">
-        {' '}
-        Articles <IoMdArrowDropdown />
-        <NavDropdown>
-          <NavItem route="/articles">See all Articles</NavItem>
-          <NavItem route="/articles/addArticle">Submit an Article</NavItem>
-        </NavDropdown>
-      </NavItem>
+      <NavItem route="/analyst">Analyst</NavItem>
+      <NavItem route="/articles">All Articles</NavItem>
+      <NavItem route="/articles/addArticle">New Article</NavItem>
     </NavBar>
   );
 };
